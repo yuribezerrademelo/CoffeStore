cddf41b4e5a80177bbdccd372b91247685585d1f
Yuri Bezerra de Melo <128265059+yuribezerrademelo@users.noreply.github.com>
2025-06-30 22:40:43 -0300

    Começando os preparativos para fazer uma função de "add itens do destaque no carrinho"

---
+++ Anotações.md
@@ -0,0 +1,5 @@
+Tenho uma ideia +- de como fazer o item entrar no carrinho(modal):
+
+1- O botão (+) vai ter uma class que eu vou declarar para cada item do destaque e dar um querySelector no js.
+2- Faço a mesma construção que fiz do input da "to do list" com a única diferença que, ao invés de puxar o valor do input e jogar no elemnto <li> criado, eu vou usar If click for em "produto1" retornar informações do produto 1, else, click em produto 2 trás produto 2......
+3- Depois de fazer isso funcionar eu tento encontrar uma forma mais "prática" de fazer isso.
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
-const modal = document.querySelector("#modal")
-const abrirmodal = document.querySelector("#abrirmodal")
+const modal = document.querySelector("#modal");
+const abrirmodal = document.querySelector("#abrirmodal");
+const addcarrinho = document.querySelector("#addcart1");
+const cafelatte = document.querySelector(".cafelatte");
 
 const togglemodal = () => modal.classList.toggle("hide");
 
 abrirmodal.addEventListener("click", togglemodal);
+
+
+// começando o código para adicionar os itens no modal
+
+const lista = document.getElementById("listacart");
+
+function adicionarnocarrinho() {
+// aqui é eu pegando o valor do + em teoria, a ideia é que o + vai  
+
+// aqui eu crio os elementos que irão ficar dentro da minha lista, e o botão de adicionar/deletar
+const item = document.createElement("li");
+const add = document.createElement("button");
+const delet = document.createElement("button");
+
+// nessa parte eu pego a nova lista criada, que vai ser o item, e jogo dentro do elemento Ul, no caso... "item" vai entrar no listacart 
+listacart.appendChild(item);
+
+
+// vou deixar esse comentado só pra caso eu precise usar em algum momento, mas eu só uso isso pra zerar um input caso eu precise... tarefa era o input e .value = "" faz ele zerar o valor 
+// document.querySelector("#tarefa").value = "";
+}
