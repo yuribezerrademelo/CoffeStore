4683a087886a7cb6bc48b7654967c98606129ce8
Yuri Bezerra de Melo <128265059+yuribezerrademelo@users.noreply.github.com>
2025-07-01 21:00:43 -0300

    um pouqinho de progresso

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 const modal = document.querySelector("#modal");
 const abrirmodal = document.querySelector("#abrirmodal");
 const addcarrinho = document.querySelector("#addcart1");
-const cafelatte = document.querySelector(".cafelatte");
 
 const togglemodal = () => modal.classList.toggle("hide");
 
@@ -12,15 +11,30 @@ abrirmodal.addEventListener("click", togglemodal);
 
 const lista = document.getElementById("listacart");
 
-function adicionarnocarrinho() {
-// aqui é eu pegando o valor do + em teoria, a ideia é que o + vai  
 
-// aqui eu crio os elementos que irão ficar dentro da minha lista, e o botão de adicionar/deletar
+addcarrinho.addEventListener("click", function(e) {
+    e.stopPropagation();
+    adicionarnocarrinho(e);
+});
+
+function adicionarnocarrinho(e) {
+
+const nome = e.target.dataset.name;
+const img = e.target.dataset.img;
+
+const texto = document.createElement("span");
 const item = document.createElement("li");
-const add = document.createElement("button");
 const delet = document.createElement("button");
+const add = document.createElement("button");
+
+texto.textContent = nome;
+add.textContent = "+";
+delet.textContent = "-";
 
 // nessa parte eu pego a nova lista criada, que vai ser o item, e jogo dentro do elemento Ul, no caso... "item" vai entrar no listacart 
+item.appendChild(texto);
+item.appendChild(add);
+item.appendChild(delet);
 listacart.appendChild(item);
 
 
